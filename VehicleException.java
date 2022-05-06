1f36701e50803f3bd634d2fbb55b042d90d51ae5
Txantoxh <105077131+Txantoxh@users.noreply.github.com>
2022-05-06 15:35:31 -0400

    Add files via upload

---
+++ VehicleException.java
@@ -0,0 +1,9 @@
+public class VehicleException extends Exception{
+  pubic VehicleException(){
+  super ("Error:vehicle needs to be selected!!");
+  
+  }
+  public VehicleException(String message){
+  super(message);
+  }
+}
